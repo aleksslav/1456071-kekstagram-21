fc1a2831664c69b0477aeb6b15322c7742f4f16f
Александр Слав <Aleksslav@gmail.com>
2020-10-21 15:05:44 +0300

    Личный проект: надо подкачаться (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 'use strict';
 
-window.picturesComponents();
-window.initPictures();
-window.picture.init();
+window.backend.load(`https://21.javascript.pages.academy/kekstagram/data`, window.pictures.init, window.errors.load);
+window.pictures.init();
